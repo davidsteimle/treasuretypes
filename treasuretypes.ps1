8dd0ea6c2b7d83e84d25b381fe28bd7575bf8dc4
David Steimle <davidsteimle@gmail.com>
2019-12-15 20:15:24 -0500

    Update treasuretypes.ps1

--- treasuretypes.ps1
+++ treasuretypes.ps1
@@ -2,6 +2,7 @@
 
 class money {
     [string]$Name
+    [string]$Acronym
     [int]$Multiplier
     [int]$Low
     [int]$High
@@ -20,3 +21,66 @@ class treasuretype {
     [string]$Magic
 }
 
+$TreasureTypes = @(
+$tta = [treasuretype]@{
+    "TreasureType" = "A"
+    "Copper" = [money]@{
+        "Name" = "copper"
+        "Acronym" = "cp"
+        "Multiplier" = 1000
+        "Low" = 1
+        "High" = 6
+        "Chance" = 25
+    }
+    "Silver" = [money]@{
+        "Name" = "silver"
+        "Acronym" = "sp"
+        "Multiplier" = 1000
+        "Low" = 1
+        "High" = 6
+        "Chance" = 30
+    }
+    "Electrum" = [money]@{
+        "Name" = "electrum"
+        "Acronym" = "ep"
+        "Multiplier" = 1000
+        "Low" = 1
+        "High" = 6
+        "Chance" = 35
+    }
+    "Gold" = [money]@{
+        "Name" = "gold"
+        "Acronym" = "gp"
+        "Multiplier" = 1000
+        "Low" = 1
+        "High" = 10
+        "Chance" = 40
+    }
+    "Platinum" = [money]@{
+        "Name" = "platinum"
+        "Acronym" = "pp"
+        "Multiplier" = 100
+        "Low" = 1
+        "High" = 4
+        "Chance" = 25
+    }
+    "Gems" = [money]@{
+        "Name" = "gem"
+        "Acronym" = ""
+        "Multiplier" = 1
+        "Low" = 4
+        "High" = 40
+        "Chance" = 60
+    }
+    "Jewelry" = [money]@{
+        "Name" = "jewelry"
+        "Acronym" = ""
+        "Multiplier" = 1
+        "Low" = 3
+        "High" = 30
+        "Chance" = 50
+    }
+    "Magic" = "Any 3: 30%"
+    
+}
+)
